a69f3900c8ae07c17ed1f3eb81255e55099e0adf
kabakae <kabakaevans@gmail.com>
2024-08-27 18:32:11 +0000

    00

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+This module contains the Flask application configured with Babel
+for internationalization and localization support.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    """
+    Configuration class for Flask app with Babel settings.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+@app.route('/')
+def index() -> str:
+    """
+    Route for the index page.
+    
+    Returns:
+        str: The rendered HTML template for the index page.
+    """
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
