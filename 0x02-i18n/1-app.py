b6304364ac8b29a83cbf426720e1f4d0ab88ed19
kabakae <kabakaevans@gmail.com>
2024-08-27 18:38:46 +0000

    00

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,6 +7,7 @@ for internationalization and localization support.
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config:
     """
     Configuration class for Flask app with Babel settings.
@@ -15,20 +16,23 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
 
+
 @app.route('/')
 def index() -> str:
     """
     Route for the index page.
-    
+
     Returns:
         str: The rendered HTML template for the index page.
     """
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
